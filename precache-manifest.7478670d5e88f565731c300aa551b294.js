842358c2e3a205a42713c2f574deb7021419aa80
Kit Aronoff <dreamnmusic@gmail.com>
2019-05-29 13:11:58 -0400

    Updates

---
+++ precache-manifest.7478670d5e88f565731c300aa551b294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e27c7da155e573d86390c28c536381",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "ec994237b2f5e98a8fb0",
+    "url": "/react-clicky-game/static/css/main.5fc874ec.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/react-clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "ec994237b2f5e98a8fb0",
+    "url": "/react-clicky-game/static/js/main.8c07743b.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
